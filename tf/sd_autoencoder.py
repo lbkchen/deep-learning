2cc1250597d2c8efced5361b9d34843ced96797b
lbkchen <lbkchen@gmail.com>
2016-07-06 12:18:02 -0700

    finish autoencoder

--- tf/sd_autoencoder.py
+++ tf/sd_autoencoder.py
@@ -16,6 +16,7 @@ from functools import wraps
 ##################
 """
 
+
 def stopwatch(f):
     """Simple decorator that prints the execution time of a function."""
     @wraps(f)
@@ -34,7 +35,9 @@ def stopwatch(f):
 ######################
 """
 
+
 class HiddenLayer(object):
+
     def __init__(self, input_tensor, n_input, n_output, W=None, b=None, act=tf.nn.tanh):
         """A hidden layer of a multi-layer perceptron. Units are fully connected
         and have activation function act. Weight matrix W is of shape
@@ -54,12 +57,13 @@ class HiddenLayer(object):
             W = tf.Variable(tf.random_uniform(
                 shape=[n_input, n_output],
                 minval=-np.sqrt(6. / (n_input + n_output)),
-                maxval=np.sqrt(6. / (n_input + n_output))
+                maxval=np.sqrt(6. / (n_input + n_output)),
+                name="W"
             ))
             W = W * 4 if act == tf.nn.sigmoid else W
 
         if b is None:
-            b = tf.Variable(tf.zeros([n_output]))
+            b = tf.Variable(tf.zeros([n_output]), name="b")
 
         self.W = W
         self.b = b
@@ -69,11 +73,116 @@ class HiddenLayer(object):
 
         self.params = [self.W, self.b]
 
+
+class DAutoencoder(object):
+
+    def __init__(self, n_visible, n_hidden, input_tensor=None, W=None,
+                 bhid=None, bvis=None, act=tf.nn.sigmoid):
+        """Initializes a denoising autoencoder by specifying the input dimension
+        and the dimension of the hidden layer. If using an SDA, inputs and weights
+        are the outputs of the previous layer.
+
+        :param n_visible: The input dimension.
+        :param n_hidden: The dimension of the hidden layer.
+        :param input_tensor: An optional input tensor variable.
+        :param W: An optional weight tensor. If the DAutoencoder is supposed to be
+            standalone, then this should be None.
+        :param bhid: An optional bias tensor for hidden units. If the DAutoencoder
+            is supposed to be standalone, then this should be None.
+        :param bvis: An optional bias tensor for visible units. If the DAutoencoder
+            is supposed to be standalone, then this should be None.
+        """
+
+        self.n_visible = n_visible
+        self.n_hidden = n_hidden
+
+        if not W:
+            W = tf.Variable(tf.random_uniform(
+                shape=[n_visible, n_hidden],
+                minval=-np.sqrt(6. / (n_visible + n_hidden)),
+                maxval=np.sqrt(6. / (n_visible + n_hidden)),
+                name="W"
+            ))
+
+        if not bvis:
+            bvis = tf.Variable(tf.zeros([n_visible]))
+
+        if not bhid:
+            bhid = tf.Variable(tf.zeros([n_hidden]), name="b")
+
+        self.W = W
+        self.b = bhid
+        self.b_prime = bvis
+        self.W_prime = tf.transpose(self.W)
+        self.act = act
+
+        self.x = input_tensor if input_tensor is not None else tf.placeholder(tf.float32)
+
+        self.params = [self.W, self.b, self.b_prime]
+
+    def get_corrupted_input(self, input_tensor, corruption_level):
+        """Keeps `1-corruption_level` proportion of entries of inputs, and zeros
+        a random subset of proportion `corruption_level` from the input.
+
+        :param input_tensor:
+        :param corruption_level: Between 0 and 1, the proportion of input to corrupt.
+        :return: The corrupted input.
+        """
+        corruption = tf.cast(tf.random_uniform(
+            shape=tf.shape(input_tensor),
+            minval=0,
+            maxval=int(1 / corruption_level),
+            dtype=tf.int32
+        ), tf.float32)
+
+        return tf.mul(input_tensor, corruption)
+
+    def get_hidden_values(self, input_tensor):
+        """Computes the values of the hidden layer.
+
+        :param input_tensor:
+        :return:
+        """
+
+        return self.act(tf.matmul(input_tensor, self.W) + self.b)
+
+    def get_reconstructed_input(self, hidden):
+        """Computes the reconstructed input given values of hidden layer.
+
+        :param hidden:
+        :return:
+        """
+
+        return self.act(tf.matmul(hidden, self.W_prime), + self.b_prime)
+
+    def get_cost_updates(self, corruption_level, learning_rate):
+        """Computes the cost and the updates for training one step of the denoising autoencoder.
+
+        :param corruption_level:
+        :param learning_rate:
+        :return:
+        """
+
+        tilde_x = self.get_corrupted_input(self.x, corruption_level)
+        y = self.get_hidden_values(tilde_x)
+        z = self.get_reconstructed_input(y)
+        cross_entropy = tf.reduce_mean(-tf.reduce_sum(
+            self.x * tf.log(z) + (1 - self.x) * tf.log(1 - z), reduction_indices=[1]))
+
+        gparams = tf.gradients(cross_entropy, self.params)
+        updates = [ #FIXME Not sure if this is the same implementation in TF
+            (param, param - learning_rate * gparam)
+            for param, gparam in zip(self.params, gparams)
+        ]
+
+        return cross_entropy, updates
+
 """
 #####################################
 ### STACKED DENOISING AUTOENCODER ###
 #####################################
 """
+
 
 class SDAutoencoder(object):
     """A stacked denoising autocoder implementation."""
