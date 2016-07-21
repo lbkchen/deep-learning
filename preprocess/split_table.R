f72a2ea9735ad747c5e96ef0ffc8d50e9ba52b37
lbkchen <lbkchen@gmail.com>
2016-07-20 22:31:39 -0700

    update split_table, looks like only one all-zero column

--- preprocess/split_table.R
+++ preprocess/split_table.R
@@ -7,11 +7,11 @@ path_train_ids <- args[2]
 path_test_ids <- args[3]
 
 print(paste("Reading", path_sam))
-sam <- fread(path_sam, header = T)
+Sam <- fread(path_sam, header = T)
 print(paste("Reading", path_train_ids))
-train_ids <- fread(path_train_ids, header = T)
+Train_ids <- fread(path_train_ids, header = T)
 print(paste("Reading", path_test_ids))
-test_ids <- fread(path_test_ids, header = T)
+Test_ids <- fread(path_test_ids, header = T)
 
 print("Done reading files.")
 
@@ -19,7 +19,31 @@ is.zero <- function(v) {
   return(v==0)
 }
 
-print(str(sam))
+unitScale <- function(v) {
+  range <- max(v) - min(v)
+  if (range == 0) {
+    return(0)
+  }
+  return((v - min(v)) / range)
+}
+
+print(str(Sam))
+
+# Test min value of Sam
+Sam.maxs <- Sam[, lapply(.SD, max)]
+print(str(Sam.maxs))
+print(sum(Sam.maxs==0))
+
+# Scale all columns of sam
+print("Starting to scale table.")
+Sam <- Sam[, lapply(.SD, unitScale)]
+
+# Split into train and test
+print("Staring to split into train and test sets.")
+Sam.train <- Sam[StatePatientID %in% Train_ids,,with=T]
+Sam.test <- Sam[StatePatientID %in% Test_ids,, with=T]
+rm(Sam)
+
 # Remove all columns with all zero entries 
-sam <- sam[,which(unlist(lapply(sam, function(x)!all(is.zero(x))))),with=F]
-print(str(sam))
+# Sam <- Sam[,which(unlist(lapply(Sam, function(x)!all(is.zero(x))))),with=F]
+# print(str(Sam))
